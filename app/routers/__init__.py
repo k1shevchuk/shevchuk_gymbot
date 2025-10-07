b50bcc046e03fa6e1bb1788496380d1faa875fff
k1shevchuk <kirill1shevchuk@gmail.com>
2025-10-07 03:43:42 +0300

    Fix settings router include naming

---
+++ app/routers/__init__.py
@@ -0,0 +1,10 @@
+from . import import_export, menu, plan, settings, summary, workout
+
+__all__ = [
+    "import_export",
+    "menu",
+    "plan",
+    "settings",
+    "summary",
+    "workout",
+]
